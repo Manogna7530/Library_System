de82fb8579ae44241760470aa8c50a65c449d586
Manogna7530 <polisettimahalakshmi2002@gmail.com>
2024-01-20 19:57:11 +0530

    Implementation in views

---
+++ lsapp/forms.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+from random import random
+from secrets import choice
+from sys import prefix
+from unicodedata import category
+from django import forms
+from numpy import require
+from lsapp import models
+from .models import EBook
+import datetime
+
+class SaveCategory(forms.ModelForm):
+    name = forms.CharField(max_length=250)
+    description = forms.Textarea()
+    status = forms.CharField(max_length=2)
+
+    class Meta:
+        model = models.Category
+        fields = ('name', 'description', 'status', )
+
+    def clean_name(self):
+        id = self.data['id'] if (self.data['id']).isnumeric() else 0
+        name = self.cleaned_data['name']
+        try:
+            if id > 0:
+                category = models.Category.objects.exclude(id = id).get(name = name, delete_flag = 0)
+            else:
+                category = models.Category.objects.get(name = name, delete_flag = 0)
+        except:
+            return name
+        raise forms.ValidationError("Category Name already exists.")
+
+class SaveBook(forms.ModelForm):
+    category = forms.CharField(max_length=250)
+    isbn = forms.CharField(max_length=250)
+    title = forms.CharField(max_length=250)
+    description = forms.Textarea()
+    author = forms.Textarea()
+    publisher = forms.Textarea()
+    date_published = forms.DateField()
+    status = forms.CharField(max_length=2)
+
+    class Meta:
+        model = models.Books
+        fields = ('isbn', 'category', 'title', 'description', 'author', 'publisher', 'date_published', 'status', )
+
+    def clean_category(self):
+        scid = int(self.data['category']) if (self.data['category']).isnumeric() else 0
+        try:
+            category = models.Category.objects.get(id = scid)
+            return category
+        except:
+            raise forms.ValidationError("Invalid Category.")
+
+    def clean_isbn(self):
+        id = int(self.data['id']) if (self.data['id']).isnumeric() else 0
+        isbn = self.cleaned_data['isbn']
+        try:
+            if id > 0:
+                book = models.Books.objects.exclude(id = id).get(isbn = isbn, delete_flag = 0)
+            else:
+                book = models.Books.objects.get(isbn = isbn, delete_flag = 0)
+        except:
+            return isbn
+        raise forms.ValidationError("ISBN already exists on the Database.")
+  
+class EBookForm(forms.ModelForm):
+    class Meta:
+        model = EBook
+        fields = '__all__'
--- lsapp/views.py
+++ lsapp/views.py
@@ -1,4 +1,195 @@
 from django.shortcuts import redirect, render
 import datetime
 import json
-# Create your views here.
+from django.contrib import messages
+from django.http import HttpResponse
+from lsapp import models, forms
+from django.db.models import Q
+from .models import Books, EBook
+from .forms import EBookForm
+
+#view
+
+def context_data(request):
+    fullpath = request.get_full_path()
+    abs_uri = request.build_absolute_uri()
+    abs_uri = abs_uri.split(fullpath)[0]
+    context = {
+        'system_host' : abs_uri,
+        'page_name' : '',
+        'page_title' : '',
+        'system_name' : 'Library System',
+        'topbar' : True,
+        'footer' : True,
+    }
+
+    return context
+
+#Home
+    
+def home(request):
+    context = context_data(request)
+    context['page'] = 'home'
+    context['page_title'] = 'Home'
+    context['categories'] = models.Category.objects.filter(delete_flag = 0, status = 1).all().count()
+    context['books'] = models.Books.objects.filter(delete_flag = 0, status = 1).all().count()
+
+    return render(request, 'home.html', context)
+
+#category
+
+def category(request):
+    context = context_data(request)
+    context['page'] = 'category'
+    context['page_title'] = "Category List"
+    context['category'] = models.Category.objects.filter(delete_flag = 0).all()
+    return render(request, 'category.html', context)
+
+
+def save_category(request):
+    resp = { 'status': 'failed', 'msg' : '' }
+    if request.method == 'POST':
+        post = request.POST
+        if not post['id'] == '':
+            category = models.Category.objects.get(id = post['id'])
+            form = forms.SaveCategory(request.POST, instance=category)
+        else:
+            form = forms.SaveCategory(request.POST) 
+
+        if form.is_valid():
+            form.save()
+            if post['id'] == '':
+                messages.success(request, "Category has been saved successfully.")
+            else:
+                messages.success(request, "Category has been updated successfully.")
+            resp['status'] = 'success'
+        else:
+            for field in form:
+                for error in field.errors:
+                    if not resp['msg'] == '':
+                        resp['msg'] += str('<br/>')
+                    resp['msg'] += str(f'[{field.name}] {error}')
+    else:
+         resp['msg'] = "There's no data sent on the request"
+
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+def view_category(request, pk = None):
+    context = context_data(request)
+    context['page'] = 'view_category'
+    context['page_title'] = 'View Category'
+    if pk is None:
+        context['category'] = {}
+    else:
+        context['category'] = models.Category.objects.get(id=pk)
+    
+    return render(request, 'view_category.html', context)
+
+
+def manage_category(request, pk = None):
+    context = context_data(request)
+    context['page'] = 'manage_category'
+    context['page_title'] = 'Manage Category'
+    if pk is None:
+        context['category'] = {}
+    else:
+        context['category'] = models.Category.objects.get(id=pk)
+    
+    return render(request, 'manage_category.html', context)
+
+
+def delete_category(request, pk = None):
+    resp = { 'status' : 'failed', 'msg':''}
+    if pk is None:
+        resp['msg'] = 'Category ID is invalid'
+    else:
+        try:
+            models.Category.objects.filter(pk = pk).update(delete_flag = 1)
+            messages.success(request, "Category has been deleted successfully.")
+            resp['status'] = 'success'
+        except:
+            resp['msg'] = "Deleting Category Failed"
+
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+#books
+
+def books(request):
+    context = context_data(request)
+    context['page'] = 'book'
+    context['page_title'] = "Book List"
+    context['books'] = models.Books.objects.filter(delete_flag = 0).all()
+    return render(request, 'books.html', context)
+
+
+def save_book(request):
+    resp = { 'status': 'failed', 'msg' : '' }
+    if request.method == 'POST':
+        post = request.POST
+        if not post['id'] == '':
+            book = models.Books.objects.get(id = post['id'])
+            form = forms.SaveBook(request.POST, instance=book)
+        else:
+            form = forms.SaveBook(request.POST) 
+
+        if form.is_valid():
+            form.save()
+            if post['id'] == '':
+                messages.success(request, "Book has been saved successfully.")
+            else:
+                messages.success(request, "Book has been updated successfully.")
+            resp['status'] = 'success'
+        else:
+            for field in form:
+                for error in field.errors:
+                    if not resp['msg'] == '':
+                        resp['msg'] += str('<br/>')
+                    resp['msg'] += str(f'[{field.name}] {error}')
+    else:
+         resp['msg'] = "There's no data sent on the request"
+
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+def view_book(request, pk = None):
+    context = context_data(request)
+    context['page'] = 'view_book'
+    context['page_title'] = 'View Book'
+    if pk is None:
+        context['book'] = {}
+    else:
+        context['book'] = models.Books.objects.get(id=pk)
+    
+    return render(request, 'view_book.html', context)
+
+
+def manage_book(request, pk = None):
+    context = context_data(request)
+    context['page'] = 'manage_book'
+    context['page_title'] = 'Manage Book'
+    if pk is None:
+        context['book'] = {}
+    else:
+        context['book'] = models.Books.objects.get(id=pk)
+    context['categories'] = models.Category.objects.filter(delete_flag = 0, status = 1).all()
+
+    return render(request, 'manage_book.html', context)
+
+
+def delete_book(request, pk = None):
+    resp = { 'status' : 'failed', 'msg':''}
+    if pk is None:
+        resp['msg'] = 'Book ID is invalid'
+    else:
+        try:
+            models.Books.objects.filter(pk = pk).update(delete_flag = 1)
+            messages.success(request, "Book has been deleted successfully.")
+            resp['status'] = 'success'
+        except:
+            resp['msg'] = "Deleting Book Failed"
+
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+
