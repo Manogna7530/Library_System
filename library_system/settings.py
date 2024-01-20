a508bf401749c4a2ef554362c2219ca473368463
Manogna7530 <polisettimahalakshmi2002@gmail.com>
2024-01-20 18:49:48 +0530

    App setting in url

--- library_system/settings.py
+++ library_system/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'lsApp.apps.lsAppConfig',
+    'lsapp.apps.lsappConfig',
 ]
 
 MIDDLEWARE = [
