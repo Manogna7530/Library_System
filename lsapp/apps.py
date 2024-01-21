e7b12c7d2d5ffc53c99161fe10fe6de4c47a6203
Manogna7530 <polisettimahalakshmi2002@gmail.com>
2024-01-21 17:35:08 +0530

    Changes and Modifications

--- lsapp/apps.py
+++ lsapp/apps.py
@@ -2,6 +2,6 @@ import imp
 from django.apps import AppConfig
 
 
-class lsappConfig(AppConfig):
+class lsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lsapp'
